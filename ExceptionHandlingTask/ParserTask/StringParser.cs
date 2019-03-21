e053198807398776b550c12ebcc4b5d5388c857c
yauheni_fiadotau <yauheni_fiadotau@epam.com>
2019-03-21 15:29:13 +0300

    add 3_module

---
+++ ExceptionHandlingTask/ParserTask/StringParser.cs
@@ -0,0 +1,24 @@
+﻿using ParserTask.Exception;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserTask
+{
+	class StringParser
+	{
+		public static int StringParse(String str)
+		{
+			try
+			{
+				return Convert.ToInt32(str);
+			}
+			catch (FormatException e)
+			{
+				throw new NotNumericException(str + " is not a number. ", e);
+			}
+		}
+	}
+}
