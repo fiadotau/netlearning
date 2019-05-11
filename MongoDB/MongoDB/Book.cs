a69b6167a075e1dbac3cca8b73d6b33bc8a81302
yauheni_fiadotau <yauheni_fiadotau@epam.com>
2019-05-11 15:22:06 +0300

    MongoDB

---
+++ MongoDB/MongoDB/Book.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDB
+{
+	class Book
+	{
+		[BsonId]
+		public ObjectId Id { get; set; }
+		public string Name { get; set; }
+		[BsonIgnoreIfDefault]
+		public string Author { get; set; }
+		public int Count { get; set; }
+		public HashSet<string> Genre { get; set; }
+		public int Year { get; set; }
+	}
+}
